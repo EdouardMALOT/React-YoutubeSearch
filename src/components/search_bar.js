93ede843ae3fc7bc6540fbb57d48f56689455be0
Edouard MLT <edouard.malot@gmail.com>
2017-02-11 11:46:32 +0100

    First commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { term: ''};
+  }
+
+  onInputChange(event) {
+    this.setState({term : event.target.value});
+  }
+
+  render(){
+    return(
+        <div>
+          <input
+            value = {this.state.term}
+            onChange={this.onInputChange.bind(this)}/>
+
+            Value of state = {this.state.term}
+        </div>
+    );
+  }
+
+}
+
+export default SearchBar;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
+const API_KEY = 'AIzaSyBXr8mchTBNQWPCbGUU89Hri3pBxGGJSN0';
+
 const rootReducer = combineReducers({
   state: (state = {}) => state
 });
